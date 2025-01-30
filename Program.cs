698fc9c8faef3e4dca906d6536da660601d36d83
Daniel <danUmukoro01@gmail.com>
2025-01-30 23:59:59 +0100

    Comment UseRedirection In program class

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 // **Apply CORS middleware before authorization**
 app.UseCors("AllowAll");
